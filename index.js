596e72185fd0ae0bcc4ee6e386674fd57c5401f7
palash <palashbnsl@gmail.com>
2015-01-10 11:56:40 +0000

    Making chnage to correctly read from file

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ var fs = require('fs');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  	fs.readFile("index.html",function(err,data){
-	if(err)
-		throw  err;
+var data = fs.readFileSync("index.html","utf-8");
+
+app.get('/', function(request, response) {	
 	response.send(data);
-	});
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
